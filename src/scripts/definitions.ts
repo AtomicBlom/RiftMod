8d13d7df012237367b0758035eec470a3ffde0bc
Steven Blom <atomicblom@binaryvibrance.net>
2018-11-05 00:17:21 +1030

    Manage portal pairs

--- src/scripts/definitions.ts
+++ src/scripts/definitions.ts
@@ -2,3 +2,28 @@ export const enum Events {
     AnnounceClient = "rift:announce_client"
 }
 
+export const enum Components {
+    RiftState = "rift:state"
+}
+
+export const enum Entity {
+    Rift = "rift:rift"
+}
+
+export interface RiftStateComponent {
+    lastLocation?: Vector;
+    partnerLocation?: Vector;
+    state: RiftState;
+    role: RiftRole;
+}
+
+export const enum RiftRole {
+    Primary = "primary",
+    Secondary = "secondary"
+}
+
+export const enum RiftState {
+    Settling = "settling",
+    WaitingForPartnerToSettle = "partnerSettle",
+    Ready = "ready"
+}
